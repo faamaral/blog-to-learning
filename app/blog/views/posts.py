80aab194f11fcac00b363d8100590e409c4f530f
faamaral <fabianoamaral445@gmail.com>
2023-10-09 19:15:51 -0300

    implemented list posts by category

--- app/blog/views/posts.py
+++ app/blog/views/posts.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
 
-from blog.database.models import Post
+from blog.database.models import Post, Category
 from blog.database.db import data
 from blog.forms.forms import CreateNewPostForm
 
@@ -10,7 +10,8 @@ posts_bp = Blueprint('posts', __name__)
 @posts_bp.route('/post/<int:id>/<string:slug>', methods=['GET'])
 def post_detail(id, slug):
     post = Post.query.filter_by(id=id).first()
-    return render_template('post_detail.html', post=post)
+    categories = Category.query.all()
+    return render_template('post_detail.html', post=post, categories=categories)
 
 @posts_bp.route('/post/new', methods=['GET', 'POST'])
 @login_required
@@ -23,3 +24,11 @@ def post_create():
         flash("Post Criado com sucesso")
         return redirect(url_for('main.index'))
     return render_template('post_detail.html', title="Crie um novo Post", form=form)
+
+@posts_bp.route('/post/category/<int:id>', methods=['GET'])
+def post_category(id):
+    # Busca todos os posts por categoria
+    category = Category.query.filter_by(id=id).first()
+    posts = Post.query.filter_by(category_id=id).all()
+    categories = Category.query.all()
+    return render_template('post_category.html', posts=posts, category=category, categories=categories  )
