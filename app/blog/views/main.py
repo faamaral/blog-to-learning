d9ffe24ca080893c84152d92c30c0373b086eb4d
faamaral <fabianoamaral445@gmail.com>
2023-10-09 02:37:34 -0300

    improve block content[

--- app/blog/views/main.py
+++ app/blog/views/main.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, render_template, send_from_directory, request, url_for
 from flask_login import current_user
 
-from blog.database.models import Post
+from blog.database.models import Post, Category
 
 
 main = Blueprint('main', __name__)
@@ -12,7 +12,17 @@ main = Blueprint('main', __name__)
 def index():
     # user = current_user.full_name
 
+    categories = Category.query.all()
+
     posts = Post.query.order_by(Post.created.desc()).all()
+
+    # page = request.args.get('page')
+    # if page and page.isdigit():
+    #     page = int(page)
+    # else:
+    #     page = 1
+
+    # pages = posts.paginate(page=page, per_page=5)
 
     
     # posts = [
@@ -26,7 +36,7 @@ def index():
     #     }
     # ]
     
-    return render_template('index.html', title='My blog', posts=posts)
+    return render_template('index.html', title='My blog', posts=posts, categories=categories)
 
 
 
